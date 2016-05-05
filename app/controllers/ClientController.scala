5d5ae70922b400667af7b3eda377d05621ec60dd
Diego Calle <dnetix@gmail.com>
2016-05-04 21:59:12 -0500

    Allows to add new clients to the database with the POST call

--- app/controllers/ClientController.scala
+++ app/controllers/ClientController.scala
@@ -14,59 +14,55 @@ import reactivemongo.play.json.collection.JSONCollection
 
 import scala.concurrent.{ExecutionContext, Future}
 
-/**
-  * A bit more complex controller using a Json Coast-to-coast approach. There is no model for Person and some data is created dynamically on creation
-  * Input is directly converted to JsObject to be stored in MongoDB
-  */
 @Singleton
 class ClientController @Inject()(val reactiveMongoApi: ReactiveMongoApi)(implicit exec: ExecutionContext) extends Controller with MongoController with ReactiveMongoComponents {
 
   def clientesFuture: Future[JSONCollection] = database.map(_.collection[JSONCollection]("Clientes"))
 
     def find = Action.async {
-    // let's do our query
-        val cursor: Future[List[JsObject]] = clientesFuture.flatMap{ clientes => //Logger.debug(clientes)
-          // find all people with name `name`
-          clientes.find(Json.obj())
-          .sort(Json.obj("NumeroDocumento" -> -1))
-          // perform the query and get a cursor of JsObject
+      val cursor: Future[List[JsObject]] = clientesFuture.flatMap{ clientes => 
+        clientes.find(Json.obj())
+          .sort(Json.obj("documento" -> -1))
           .cursor[JsObject](ReadPreference.primary).collect[List]()
       }
       cursor.map { clientes =>
-          Ok(Json.toJson(clientes)).withHeaders(ACCESS_CONTROL_ALLOW_ORIGIN -> "*")
-        }
+        Ok(Json.toJson(clientes)).withHeaders(ACCESS_CONTROL_ALLOW_ORIGIN -> "*")
+      }
     }
-    def create(NombreCompleto: String,tipoDocumento: String, numeroDocumento:Int,ejecutivoAcargo:String) = Action.async {
-    val json = Json.obj(
-      "NombreCompleto" -> NombreCompleto,
-      "tipoDocumento" -> tipoDocumento,
-      "NumeroDocumento" -> numeroDocumento,
-      "ejecutivoAcargo" -> ejecutivoAcargo,
-      "productos" -> "[]")
 
-    for {
-      clientes <- clientesFuture
-      lastError <- clientes.insert(json)
-    } yield Ok("Mongo LastError: %s".format(lastError))
+    def create = Action.async { request => 
+      
+      val json = Json.obj(
+        "nombre_completo" -> request.body.asFormUrlEncoded.get("nombre_completo")(0),
+        "tipo_doc" -> request.body.asFormUrlEncoded.get("tipo_doc")(0),
+        "documento" -> request.body.asFormUrlEncoded.get("documento")(0),
+        "correo" -> request.body.asFormUrlEncoded.get("correo")(0),
+        "ejecutivo_encargado" -> request.body.asFormUrlEncoded.get("ejecutivo_encargado")(0),
+        "productos" -> "[]"
+      )
+
+      for {
+        clientes <- clientesFuture
+        lastError <- clientes.insert(json)
+      } yield Ok("Mongo LastError: %s" . format(lastError))
 
   }
 
-    def findById(tipoDocumento:String,numeroDocumento: Int) = Action.async {
-    // let's do our query
+
+  def findById(documentType:String, documentNumber:String) = Action.async {
+    Logger.info(documentType);
     val cursor: Future[List[JsObject]] = clientesFuture.flatMap{ clientes =>
       // find all people with name `name`
-      clientes.find(Json.obj("tipoDocumento"->tipoDocumento,"NumeroDocumento" -> numeroDocumento)).
+      clientes.find(Json.obj("tipo_doc" -> documentType, "documento" -> documentNumber)).
       // sort them by creation date
-      sort(Json.obj("NumeroDocumento" -> -1)).
+      sort(Json.obj("documento" -> -1)).
       // perform the query and get a cursor of JsObject
       cursor[JsObject](ReadPreference.primary).collect[List]()
-  }
+    }
 
-    // everything's ok! Let's reply with a JsValue
     cursor.map { clientes =>
       Ok(Json.toJson(clientes)).withHeaders(ACCESS_CONTROL_ALLOW_ORIGIN -> "*")
     }
   }
-
 
 }
